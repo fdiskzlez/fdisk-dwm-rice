16207af82c03f9b68c07663b7717105d2f293852
quinlanfab <65056928+quinlanfab@users.noreply.github.com>
2021-06-07 12:07:28 -0400

    Update config.h

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "tewi:size=12", "tewi:size=12" };
+static const char dmenufont[]       = "tewi:size=12";
 static const char col_1[]       = "#282a36";
 static const char col_2[]       = "#ffb86c";
 static const char col_3[]       = "#ff79c6";
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "main", "development", "other", };
 
 static const Rule rules[] = {
 	/* xprop(1):
